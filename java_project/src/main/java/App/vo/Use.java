6fd84aa1f76c64d6467b0765e9b9ed35f2092447
Donghyun Oh <dhgus44@naver.com>
2023-06-09 00:50:25 +0900

    재수정

--- java_project/src/main/java/App/vo/Use.java
+++ java_project/src/main/java/App/vo/Use.java
@@ -6,16 +6,9 @@ public class Use {
     final static int SIZE = 50000;
     public static int menu = 0;
     public static int cnt = 0; // 사용한 곳 
-    public static int moneyToAdd = 0; // 추가 용돈
+
     public static String[] where = new String[SIZE];
     public static int[] a = new int[SIZE];
     public static int money = 0; //처음 용돈
-
-    public void setWhere(String where){
-
-    }
-    public void setExpense(int a){
-
-    }
 
 }
