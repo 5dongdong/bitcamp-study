f39cd22eda96431f71f129b92e6d26a5f3e33dee
Odongdong <dhgus44@naver.com>
2023-06-13 20:37:09 +0900

    코드 추가

--- myapp/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
@@ -55,45 +55,44 @@ public class BoardHandler {
   }
 
   public static void updateBoard() {
-    // String BoardNo = Prompt.inputString("번호? ");
-    // for (int i = 0; i < length; i++) {
-    // Board m = boards[i];
-    // if (m.getNo() == Integer.parseInt(BoardNo)) {
-    // System.out.printf("이름(%s)? ", m.getName());
-    // m.setName(Prompt.inputString(""));
-    // System.out.printf("이메일(%s)? ", m.getEmail());
-    // m.setEmail(Prompt.inputString(""));
-    // System.out.printf("새암호? ");
-    // m.setPassword(Prompt.inputString(""));
-    // m.setGender(inputGender(m.getGender()));
-    // return;
-    // }
-    // }
-    // System.out.println("해당 번호의 회원이 없습니다!");
+    String BoardNo = Prompt.inputString("번호? ");
+    for (int i = 0; i < length; i++) {
+      Board board = boards[i];
+      if (board.getNo() == Integer.parseInt(BoardNo)) {
+        System.out.printf("제목(%s)? ", board.getTitle());
+        board.setTitle(Prompt.inputString(""));
+        System.out.printf("내용(%s)? ", board.getContent());
+        board.setContent(Prompt.inputString(""));
+        System.out.printf("새암호? ");
+        board.setPassword(Prompt.inputString(""));
+        return;
+      }
+    }
+    System.out.println("해당 번호의 게시글이 없습니다!");
   }
 
 
 
   public static void deleteBoard() {
-    // int BoardNo = Prompt.inputInt("번호? ");
-    //
-    // int deletedIndex = indexOf(BoardNo);
-    // if (deletedIndex == -1) {
-    // System.out.println("해당 번호의 회원이 없습니다!");
-    // return;
-    // }
-    //
-    // for (int i = deletedIndex; i < length - 1; i++) {
-    // boards[i] = boards[i + 1];
-    // }
-    //
-    // boards[--length] = null;
+    int BoardNo = Prompt.inputInt("번호? ");
+
+    int deletedIndex = indexOf(BoardNo);
+    if (deletedIndex == -1) {
+      System.out.println("해당 번호의 게시글이 없습니다!");
+      return;
+    }
+
+    for (int i = deletedIndex; i < length - 1; i++) {
+      boards[i] = boards[i + 1];
+    }
+
+    boards[--length] = null;
   }
 
   private static int indexOf(int BoardNo) {
     for (int i = 0; i < length; i++) {
-      Board m = boards[i];
-      if (m.getNo() == BoardNo) {
+      Board board = boards[i];
+      if (board.getNo() == BoardNo) {
         return i;
       }
     }
