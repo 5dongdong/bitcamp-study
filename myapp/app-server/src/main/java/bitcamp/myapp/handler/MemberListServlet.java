9ffdd35d487908c29dbc3ea7d6d5b6f8979d4fbe
Odongdong <dhgus44@naver.com>
2023-08-14 18:23:33 +0900

    23/08/14
    수업내용

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberListServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberListServlet.java
@@ -21,6 +21,7 @@ public class MemberListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+	    response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
---
+++ project/project-server/src/main/java/bitcamp/myapp/handler/AnnounceListServlet.java
@@ -0,0 +1,24 @@
+package bitcamp.myapp.handler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/anncoune/list")
+public class AnnounceListServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println;
+	}
+}
